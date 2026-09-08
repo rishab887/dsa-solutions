a0c6f42b4d7aad1cfa888549143e8ffb72ccb381
Rishabh Gautam <rishabhgautam668@gmail.com>
2026-09-08 21:36:11 +0530

    Time: 0 ms (100%), Space: 20.7 MB (52.5%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
@@ -1,20 +1,36 @@
 class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
-        vector<int> even;
-        vector<int> odd;
-        for(int i = 0 ; i < nums.size() ; i++){
-            if(nums[i]%2 == 0){
-                even.push_back(nums[i]);
+        // vector<int> even;
+        // vector<int> odd;
+        // for(int i = 0 ; i < nums.size() ; i++){
+        //     if(nums[i]%2 == 0){
+        //         even.push_back(nums[i]);
+        //     }else{
+        //         odd.push_back(nums[i]);
+        //     }
+        // }
+        // for(int i = 0 ; i < even.size() ; i++){
+        //     nums[i] = even[i];
+        // }
+        // for(int i = even.size() ; i < even.size() + odd.size() ; i++){
+        //     nums[i] = odd[i - even.size()];
+        // }
+        int left = 0;
+        int right = nums.size() - 1;
+        while(left < right){
+            if(nums[right] % 2 == 0 && nums[left] % 2 != 0){
+                swap(nums[left],nums[right]);
+                right--;
+                left++;
+            }else if(nums[right] % 2 == 0 &&  nums[left] % 2 == 0){
+                left++;
+            }else if(nums[left] % 2 != 0){
+                right--;
             }else{
-                odd.push_back(nums[i]);
+                right--;
+                left++;
             }
-        }
-        for(int i = 0 ; i < even.size() ; i++){
-            nums[i] = even[i];
-        }
-        for(int i = even.size() ; i < even.size() + odd.size() ; i++){
-            nums[i] = odd[i - even.size()];
         }
         return nums;
     }
