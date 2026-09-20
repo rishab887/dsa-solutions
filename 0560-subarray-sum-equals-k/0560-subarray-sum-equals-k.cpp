ad28754bb4322ba908c7a8659c02ae1dc6ac140a
Rishabh Gautam <rishabhgautam668@gmail.com>
2026-09-20 13:05:01 +0530

    Time: 39 ms (70.88%), Space: 51.1 MB (8.19%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -4,21 +4,28 @@ public:
         unordered_map<int,int> mp;
         int sum = 0;
         int count = 0;
+        mp[0] = 1;
         for(int i = 0 ; i < nums.size() ; i++){
-            
             sum += nums[i];
-            if(sum == k){
-                count++;
-            }
-            int rem = sum - k ;
-            if(mp.find(rem) != mp.end()){
-                count += mp[rem];
-            }
-            // if(mp.find(sum) == mp.end()){
-            //     mp[sum] = i;
-            // }
+            int rem = sum - k;
+            count += mp[rem];
             mp[sum]++;
         }
+        // for(int i = 0 ; i < nums.size() ; i++){
+            
+        //     sum += nums[i];
+        //     if(sum == k){
+        //         count++;
+        //     }
+        //     int rem = sum - k ;
+        //     if(mp.find(rem) != mp.end()){
+        //         count += mp[rem];
+        //     }
+        //     // if(mp.find(sum) == mp.end()){
+        //     //     mp[sum] = i;
+        //     // }
+        //     mp[sum]++;
+        // }
         return count;
     }
 };
